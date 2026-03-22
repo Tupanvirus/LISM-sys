66c8f59685bcc5abcd21a6f53b51fe49167fd20a
Tupanvirus <andrewgusfill07092006@gmail.com>
2026-03-22 22:32:33 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ import qrcode
 import io
 
 #Настройки Supabase
-SUPABASE_URL = st.secrets[SUPABASE_URL]
-SUPABASE_KEY = st.secrets[SUPABASE_KEY]
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 st.write(st.secrets)
 # Функции ГОСТ
